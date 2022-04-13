a05bf9cd0904fef70276078ea4021ab6ac9aab3f
Zubair Farahi <zubair.farahi121@yahoo.com>
2022-04-13 09:58:39 +0300

    done

---
+++ challanges/js/script.js
@@ -0,0 +1,23 @@
+/*
+    Task: Capture the user's search query
+    1. Add an event listener for 'keyup'
+    2. Capture the event in a 'let' called 'searchQuery' - storing the value
+    3. Make sure the captured value is lowercase
+    4. Log to the console to demonstrate it works
+*/
+
+
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+        
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
+    }
+});
